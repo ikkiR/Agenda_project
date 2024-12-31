2757babf849b1034c5661c92f9c791737cc3cb25
Kaiqui Rocha da Silva <kaiquirocha595@outlook.com>
2024-12-31 19:42:48 -0300

    realizando as alterações

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
+
+
+
 class Category (models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -21,6 +29,7 @@ class Contact(models.Model):
     show = models.BooleanField(default = True)
     picture = models.ImageField(blank = True, upload_to = 'pictures/%y/%m/')
     category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank = True, null = True)
+    owner =  models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
 
 
     def __str__(self) -> str:
