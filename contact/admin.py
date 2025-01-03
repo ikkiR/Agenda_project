5c82f2f70619431c6cc2beae709252e48a4dca7d
kaiquirocha <kaiquirocha595@outlook.com>
2025-01-02 22:29:01 -0300

    alterando nosso css e concertando erros de sintade da pep8

--- contact/admin.py
+++ contact/admin.py
@@ -6,9 +6,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
-    #list_filter = 'created_date',
+    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 50
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(
         request,
         'contact/index.html',
     )
+
