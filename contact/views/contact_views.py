3abd783edbe8e3d9475f6ed7c60bbc048ae922e7
kaiquirocha <kaiquirocha595@outlook.com>
2025-01-03 00:53:55 -0300

    criando alguns partials

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
     return render(
         request,
@@ -21,8 +22,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True) # noqa
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title, 
     }
     return render(
         request,
