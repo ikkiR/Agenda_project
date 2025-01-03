adada674a7f9ad893eada7fcc0a3ece173f5e3d8
Kaiqui Rocha da Silva <kaiquirocha595@outlook.com>
2025-01-02 21:24:34 -0300

    criando o utils para dados do fakar

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
-
+# flake8: noqa
 
 
 
@@ -29,7 +29,8 @@ class Contact(models.Model):
     show = models.BooleanField(default = True)
     picture = models.ImageField(blank = True, upload_to = 'pictures/%y/%m/')
     category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank = True, null = True)
-    owner =  models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,blank =True,null =True)
+
 
 
     def __str__(self) -> str:
