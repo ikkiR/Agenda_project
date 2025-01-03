e3b669568d20d3df95ba94f00864e2e9956b3443
kaiquirocha <kaiquirocha595@outlook.com>
2025-01-02 23:35:05 -0300

    alterando o html da pagina index

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
-# flake8: noqa
-
 
 
 class Category (models.Model):
@@ -17,21 +15,18 @@ class Category (models.Model):
         return f'{self.name}'
 
 
-
-
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
-    created_date = models.DateTimeField(default = timezone.now)
-    description = models.TextField(blank = True)
-    show = models.BooleanField(default = True)
-    picture = models.ImageField(blank = True, upload_to = 'pictures/%y/%m/')
-    category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank = True, null = True)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL,blank =True,null =True)
-
-
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True) # noqa
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True) # noqa
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+    
