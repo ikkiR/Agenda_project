037e4ebcd1988119f207b690e1960067f5117329
kaiquirocha <kaiquirocha595@outlook.com>
2025-01-17 00:34:26 -0300

    teste2

--- project/settings.py
+++ project/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR /  "base_templates"
+            BASE_DIR /  "base_templates", 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
